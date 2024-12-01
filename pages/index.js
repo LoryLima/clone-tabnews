8309ebf42ef18b90a9f13dd03b9bb435765c7213
Lorena <97991607+LoryLima@users.noreply.github.com>
2024-12-01 21:22:05 +0000

    font test

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
         color: '#FFD700',
         textAlign: 'center',
         fontSize: '3rem',
+        fontFamily: 'Helvetica, sans-serif'
     };
 
     return <h1 style={headingStyle}>Testing Vercel again</h1>;
