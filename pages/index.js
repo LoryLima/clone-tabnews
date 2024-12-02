76fa2d8faac0e1fba2e58b1dbb54bab2fb15832d
Lorena <97991607+LoryLima@users.noreply.github.com>
2024-12-02 03:25:23 +0000

    uma versao mais simples

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
 function Home() {
-    const headingStyle = {
-        color: '#000000',
-        textAlign: 'center',
-        fontSize: '3rem',
-        fontFamily: 'Roboto, sans-serif'
-    };
 
-    return <h1 style={headingStyle}>Texto no Centro</h1>;
+    return <h1>Apenas um texto</h1>;
 }
 
 export default Home;
