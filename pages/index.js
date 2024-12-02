2ea81fd93388e3c0839630bd3c5ff3d51f5a913e
Lorena <97991607+LoryLima@users.noreply.github.com>
2024-12-02 04:12:03 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
+  return <h1>Apenas um texto</h1>;
+}
 
-    return <h1>Apenas um texto</h1>;
+function test() {
+  console.log("teste");
+}
+
+function test2() {
+  console.log("teste 2");
 }
 
 export default Home;
